7931407b4423f54621c6fd3c38b147e112cf1fb9
Constantinus_Y <Constantinus_Y@163.com>
2020-06-20 12:04:07 +0800

    发现上次并没有完全解决问题，准备直接在README里面声明请勿关闭窗口

--- Developing/sound_play.py
+++ Developing/sound_play.py
@@ -1,16 +1,7 @@
-import psutil
-import sys
+
 from pygame import mixer
 mixer.init()
 mixer.music.load('BGM.wav')
 while True:
-    Pl = psutil.pids()
-    for processes in Pl:
-        if psutil.Process(processes).name() == 'Launcher.exe':
-            if not mixer.music.get_busy():
-                mixer.music.play()
-            break
-        else:
-            pass
-    else:
-        exit()
+    if not mixer.music.get_busy():
+        mixer.music.play()
