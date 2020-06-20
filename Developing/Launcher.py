acfa24b0f019e44aae1619b6c4360335df4804bf
Constantinus_Y <Constantinus_Y@163.com>
2020-06-20 12:09:24 +0800

    添加注释。

--- Developing/Launcher.py
+++ Developing/Launcher.py
@@ -1,26 +1,27 @@
-import multiprocessing
+import multiprocessing  # 用这个库实现并行
 import atexit
 
 from multiprocessing import Process
-import os
+import os  # 用这个库打开Exe并在结束时杀死进程
 
 
-@atexit.register
+@atexit.register  # 这个装饰器是用来在程序结束后杀死进程，解决背景音乐在后台播放的问题
 def clean():
-    os.system("taskkill /im sound_play.exe /f")
+    os.system("taskkill /im sound_play.exe /f")  # 使用os杀死进程
     os.system("taskkill /im Launcher.exe /f")
 
 
-def processA():
+def processA():  # 准备好A进程
     os.system("kanas_review.exe")
 
 
-def processB():
+def processB():  # 准备好B进程
     os.system("sound_play.exe")
 
 
 if __name__ == '__main__':
-    multiprocessing.freeze_support()
+    multiprocessing.freeze_support()  # 防止压制的exe运行异常
+    # 以下是任务并行的实现
     p = Process(target=processA)
     q = Process(target=processB)
     p.start()
--- Developing/sound_play.py
+++ Developing/sound_play.py
@@ -1,7 +1,9 @@
+from pygame import mixer  # 基于Pygame播放背景音乐
 
-from pygame import mixer
-mixer.init()
-mixer.music.load('BGM.wav')
+mixer.init()  # 初始化对象
+mixer.music.load('BGM.wav')  # 加载音乐
+
+# 无限循环播放
 while True:
-    if not mixer.music.get_busy():
-        mixer.music.play()
+    if not mixer.music.get_busy():  # 如果音乐没在播放
+        mixer.music.play()  # 重新开始播放
